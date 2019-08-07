8da12996d0c9c78f2a0182c995bfa3e9ff4eca4e
Matthew <mngor@ucsc.edu>
2019-08-07 03:00:42 -0700

    onChange instead of defaultChecked way, may be redundant lol

--- src/App.js
+++ src/App.js
@@ -11,12 +11,34 @@ class App extends React.Component{
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  
+  handleChange(id){
+    this.setState((prevState) => {  
+      const updatedData = prevState.todos.map((todo) => {
+        if (todo.id === id){
+          todo.completed = !todo.completed 
+          /*
+            above, 
+            - this is not good practice. don't directly modify the original todo object.
+            - should create a copy of the original todo before making any modifications.
+          */ 
+        }
+        return todo // need to return b/c that's how the map() function works
+        // Whatever you return from the callback function is the value that will be 
+        // put into the new array at the same index as the item you're looking at from the original array.
+      })
+      return{
+        todos: updatedData
+      }
+    })
   }
 
   render(){
     const todoItems = this.state.todos.map((item) => {
       return(
-        <TodoItem key = {item.id} item = {item}/>
+        <TodoItem key = {item.id} item = {item} handleChange = {this.handleChange}/>
       )
     })
 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,22 @@ import './TodoItem.css';
 function TodoItem(props){
     return(
         <div className = "todo-item">
-            <input type="checkbox" defaultChecked = {props.item.completed}/>
+            <input 
+                type = "checkbox" 
+                //defaultChecked = {props.item.completed} // just another way to do all of this?
+                checked = {props.item.completed}
+                onChange = {() => props.handleChange(props.item.id)}
+                /* above, 
+                    - We need to create an anonymous inline function 
+                    b/c we don't get to decide the parameter being passed to the event handler.
+                    - (The browser will always pass the event objects to event handlers).
+                    - We want to pass the id of the todo to handleChange, not the event object. 
+                    - We could take the event as a parameter if we wanted, but we didn't since
+                    we don't use it. - e.g: (event) => props.handleChange(props.item.id)
+                    - This allows us to pass whatever value we want to our function, instead of being
+                    forced to accept the event object.
+                */
+            />
             <p>{props.item.text}</p>
         </div>
     )
